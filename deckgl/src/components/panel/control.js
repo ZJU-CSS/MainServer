94e856f7a6f37ce977d98a28bff9863db998c5af
zhiguangda <317132773@qq.com>
2020-04-22 11:34:10 +0800

    '422'

--- deckgl/src/components/panel/control.js
+++ deckgl/src/components/panel/control.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import { Select,SIZE } from "baseui/select";
@@ -13,7 +13,7 @@ import RadioGroup from "@material-ui/core/RadioGroup";
 import Radio from "@material-ui/core/Radio";
 import FormLabel from "@material-ui/core/FormLabel";
 
-let keys=[];
+// let keys=[];
 let ip={}
 let content={}
 function readData(){
@@ -27,17 +27,23 @@ function readData(){
 }
 
 function setK() {
-    d3.csv('./content.csv').then(d=>{
-        d.map(x=>{
-            content[x['name']]=x['path']
+    return new Promise(resolve => {
+        d3.csv('https://raw.githubusercontent.com/HOOLoLo/webserver/master/content.csv').then(d=>{
+            d.map(x=>{
+                content[x['name']]=x['path']
+            })
+            console.log(content)
+
+
+            let k=Object.keys(content);
+            console.log('k',k)
+            resolve(k)
         })
-        console.log(content)
-
-        keys=Object.keys(content)
-
     })
 }
-setK()
+
+// setK().then()
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -49,9 +55,40 @@ const useStyles = makeStyles((theme) => ({
 
 
 
+
 readData()
+
+
+
+
+
+// export class Panel extends Component{
+//
+//     constructor(props){
+//         super(props)
+//         keys=setK()
+//
+// }
+//
+// render() {
+//
+// }
+//
+//
+// }
+let p=setK();
+
+
 export function Panel() {
     const [value, setValue] = React.useState('MIC');
+
+    const[keys,setKeys]=React.useState(['wait']);
+    p.then((result)=>{
+        setKeys(result)
+    })
+    // setKeys(setK());
+
+
     const classes = useStyles();
     function handleChange(event) {
         console.log()
@@ -71,45 +108,70 @@ export function Panel() {
 
     }
 
+    // if(!keys){
+    //     console.log('keys')
+    //     return <div>wait</div>
+    // }
 
     return (
-        <FormControl component="fieldset">
-            <FormLabel component="legend">Content</FormLabel>
-            <RadioGroup aria-label="content" name="content" value={value} onChange={handleChange}>
-                <FormControlLabel value={keys[0]} control={<Radio />} label={keys[0]}  />
-                <FormControlLabel value={keys[1]}  control={<Radio />} label={keys[1]} />
-                <FormControlLabel value={keys[2]} control={<Radio />} label={keys[2]}  />
-                <FormControlLabel value={keys[3]} control={<Radio />} label={keys[3]} />
-                <FormControlLabel value={keys[4]} control={<Radio />} label={keys[4]} />
-            </RadioGroup>
-            <div className={classes.root}>
-            <Button variant="contained" color="primary" value={1} onClick={send} >
-                1号机(MIC)
-            </Button>
-            <Button variant="contained" color="primary" value={2} onClick={send}>
-                2号机(ML)
-            </Button>
-            <Button variant="contained" color="primary" value={3} onClick={send}>
-                3号机(DTW)
-            </Button>
-            <Button variant="contained" color="primary" value={4} onClick={send}>
-                4号机(news)
-            </Button>
-            <Button variant="contained" color="primary" value={5} onClick={send}>
-                5号机(conclusion)
-            </Button>
-            <Button variant="contained" color="primary" value={6} onClick={send}>
-                6号机(融合)
-            </Button>
-                <Button variant="contained" color="primary" value={7} onClick={send}>
-                    7号机(玻璃)
-                </Button>
-                <Button variant="contained" color="primary" value={8} onClick={send}>
-                    8号机(窗户)
-                </Button>
-            </div>
-        </FormControl>
+            <FormControl component="fieldset">
+                <FormLabel component="legend">Content</FormLabel>
+                <RadioGroup aria-label="content" name="content" value={value} onChange={handleChange}>
+                    {/*{*/}
+                    {/*    ()=>{*/}
+                    {/*        if(keys!==[]){*/}
+                    {/*            console.log(keys);*/}
+                    {/*   */}
+                    {/*        }*/}
+                    {/*    }*/}
+                    {/*}*/}
+                    {
+                        keys.map((value,index)=>{
+                            console.log('d',value);
+                            return(
+                                <FormControlLabel key={index} value={value} control={<Radio />} label={value}  />
+                            )
+                        })
+                    }
 
-    );
+
+                    {/*<FormControlLabel value={keys[0]} control={<Radio />} label={d}  />*/}
+                    {/*<FormControlLabel value={d} control={<Radio />} label={d}  />*/}
+                    {/*<FormControlLabel value={d} control={<Radio />} label={d}  />*/}
+                    {/*<FormControlLabel value={d} control={<Radio />} label={d}  />*/}
+                    {/*<FormControlLabel value={d} control={<Radio />} label={d}  />*/}
+                </RadioGroup>
+                <div className={classes.root}>
+                    <Button variant="contained" color="primary" value={1} onClick={send} >
+                        1号机(MIC)
+                    </Button>
+                    <Button variant="contained" color="primary" value={2} onClick={send}>
+                        2号机(ML)
+                    </Button>
+                    <Button variant="contained" color="primary" value={3} onClick={send}>
+                        3号机(DTW)
+                    </Button>
+                    <Button variant="contained" color="primary" value={4} onClick={send}>
+                        4号机(news)
+                    </Button>
+                    <Button variant="contained" color="primary" value={5} onClick={send}>
+                        5号机(conclusion)
+                    </Button>
+                    <Button variant="contained" color="primary" value={6} onClick={send}>
+                        6号机(融合)
+                    </Button>
+                    <Button variant="contained" color="primary" value={7} onClick={send}>
+                        7号机(玻璃)
+                    </Button>
+                    <Button variant="contained" color="primary" value={8} onClick={send}>
+                        8号机(窗户)
+                    </Button>
+                </div>
+            </FormControl>
+
+        );
+
+
+
 }
 
