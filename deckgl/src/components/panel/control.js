e72e28058ffef510fbbf529fb396801544828c1c
zhiguangda <317132773@qq.com>
2020-04-22 12:06:19 +0800

    '中文'

--- deckgl/src/components/panel/control.js
+++ deckgl/src/components/panel/control.js
@@ -16,12 +16,19 @@ import FormLabel from "@material-ui/core/FormLabel";
 // let keys=[];
 let ip={}
 let content={}
-function readData(){
-    d3.csv('./panel.csv').then(d=>{
-        d.map(x=>{
-            ip[x['no']]=x['ip']
+//读取ip地址
+function readipData(){
+
+    return new Promise(resolve => {
+        d3.csv('./panel.csv').then(d=>{
+            d.map(x=>{
+                ip[x['no']]=x['ip']
+            })
+            let name=Object.keys(ip);
+            console.log(name)
+            resolve(name)
+            // console.log(ip)
         })
-        console.log(ip)
     })
 
 }
@@ -33,8 +40,6 @@ function setK() {
                 content[x['name']]=x['path']
             })
             console.log(content)
-
-
             let k=Object.keys(content);
             console.log('k',k)
             resolve(k)
@@ -56,7 +61,6 @@ const useStyles = makeStyles((theme) => ({
 
 
 
-readData()
 
 
 
@@ -77,15 +81,22 @@ readData()
 //
 // }
 let p=setK();
-
-
+let p2=readipData()
 export function Panel() {
     const [value, setValue] = React.useState('MIC');
 
     const[keys,setKeys]=React.useState(['wait']);
+    const[ipName,setName]=React.useState(['wait']);
+
     p.then((result)=>{
         setKeys(result)
+    });
+
+    p2.then((result)=>{
+        setName(result)
     })
+
+
     // setKeys(setK());
 
 
@@ -142,30 +153,40 @@ export function Panel() {
                     {/*<FormControlLabel value={d} control={<Radio />} label={d}  />*/}
                 </RadioGroup>
                 <div className={classes.root}>
-                    <Button variant="contained" color="primary" value={1} onClick={send} >
-                        1号机(MIC)
-                    </Button>
-                    <Button variant="contained" color="primary" value={2} onClick={send}>
-                        2号机(ML)
-                    </Button>
-                    <Button variant="contained" color="primary" value={3} onClick={send}>
-                        3号机(DTW)
-                    </Button>
-                    <Button variant="contained" color="primary" value={4} onClick={send}>
-                        4号机(news)
-                    </Button>
-                    <Button variant="contained" color="primary" value={5} onClick={send}>
-                        5号机(conclusion)
-                    </Button>
-                    <Button variant="contained" color="primary" value={6} onClick={send}>
-                        6号机(融合)
-                    </Button>
-                    <Button variant="contained" color="primary" value={7} onClick={send}>
-                        7号机(玻璃)
-                    </Button>
-                    <Button variant="contained" color="primary" value={8} onClick={send}>
-                        8号机(窗户)
-                    </Button>
+
+                    {
+                        ipName.map(((value, index) => {
+                            return(
+                                <Button key={index} variant="contained" color="primary" value={value} onClick={send} >
+                                    {value}
+                                </Button>
+                            )
+                        }))
+                    }
+                    {/*<Button variant="contained" color="primary" value={1} onClick={send} >*/}
+                    {/*    1号机(MIC)*/}
+                    {/*</Button>*/}
+                    {/*<Button variant="contained" color="primary" value={2} onClick={send}>*/}
+                    {/*    2号机(ML)*/}
+                    {/*</Button>*/}
+                    {/*<Button variant="contained" color="primary" value={3} onClick={send}>*/}
+                    {/*    3号机(DTW)*/}
+                    {/*</Button>*/}
+                    {/*<Button variant="contained" color="primary" value={4} onClick={send}>*/}
+                    {/*    4号机(news)*/}
+                    {/*</Button>*/}
+                    {/*<Button variant="contained" color="primary" value={5} onClick={send}>*/}
+                    {/*    5号机(conclusion)*/}
+                    {/*</Button>*/}
+                    {/*<Button variant="contained" color="primary" value={6} onClick={send}>*/}
+                    {/*    6号机(融合)*/}
+                    {/*</Button>*/}
+                    {/*<Button variant="contained" color="primary" value={7} onClick={send}>*/}
+                    {/*    7号机(玻璃)*/}
+                    {/*</Button>*/}
+                    {/*<Button variant="contained" color="primary" value={8} onClick={send}>*/}
+                    {/*    8号机(窗户)*/}
+                    {/*</Button>*/}
                 </div>
             </FormControl>
 
